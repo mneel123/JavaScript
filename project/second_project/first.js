fd17e3dfeeaeaa2f2e356bc3a180cb414e575e11
Mr_Yash_0602 <113755566+mneel123@users.noreply.github.com>
2023-12-15 10:55:42 +0530

    Add files via upload

---
+++ project/second_project/first.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('form')
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+   const height = parseInt(document.querySelector('#height').value)
+   const weight = parseInt(document.querySelector('#weight').value)
+   const result = document.querySelector('#results');
+
+   if(height === '' || height < 0 || isNaN(height)){
+    result.innerHTML = `plese give me a valid height ${height}`;
+   }else if (weight === '' || weight < 0 || isNaN(weight)){
+    result.innerHTML = `plese give me a valid height ${weight}`;
+   }else {
+    const bmi = (weight / ((height*height)/10000)).toFixed(2)
+    results.innerHTML = `<span>${bmi}</span>`;
+   }
+   
+})
